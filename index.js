1652cdc2664fa8b05a0e7b272efda26f9f6447f7
PetarAsenov <p.a.asenov@gmail.com>
2020-02-20 15:54:02 +0100

    add joke

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express()
 const page = `<html>
   <head>
     <title>Petar Asenov</title>
+    <style>
+      body {
+        background-color: blue;
+        color: white;
+      }
+    </style>
   </head>
   <body>
-    <h1>Why do comedians love !false?</h1>
-    <p>It's funny because it's true.</p>
+    <h1>What's the similarity between Amsterdam and the Tour de France?</h1>
+    <p>Both have a lot of people on bicycles doing drugs.</p>
   </body>
 
 </html>`
